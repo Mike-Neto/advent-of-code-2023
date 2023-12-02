22489cb0124a61bb482e4cfdb632ae643b79a4f1
Miguel Mendes <miguelmendeseiriz@gmail.com>
2023-12-02 19:04:23 +0000

    par impl for part 1

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -1,9 +1,8 @@
 use day_02::part1::process;
 
-fn main() -> anyhow::Result<()> {
+fn main() {
     let file = include_str!("../../data.txt");
-    let result = process(file)?;
+    let result = process(file);
     println!("The sum of the IDs of those games is: {result}");
     assert_eq!(result, 2207);
-    Ok(())
 }
--- day-02/src/part1.rs
+++ day-02/src/part1.rs
@@ -1,31 +1,34 @@
-use crate::parse_games;
+use rayon::{iter::ParallelIterator, str::ParallelString};
+
+use crate::parse_game;
 
 static BAG_RED: u64 = 12;
 static BAG_GREEN: u64 = 13;
 static BAG_BLUE: u64 = 14;
 
-/// # Errors
-///
-/// Will return `Err` parsing fails
-pub fn process(input: &str) -> anyhow::Result<u64> {
-    #[allow(clippy::redundant_closure_for_method_calls)]
-    let (_, games) = parse_games(input).map_err(|err| err.to_owned())?;
-
-    Ok(games
-        .into_iter()
-        .filter_map(|g| {
-            if !g.sets.into_iter().any(|s| {
-                s.into_iter().any(|c| match c.color {
-                    crate::Color::Red => c.number > BAG_RED,
-                    crate::Color::Green => c.number > BAG_GREEN,
-                    crate::Color::Blue => c.number > BAG_BLUE,
-                })
-            }) {
-                return Some(g.id);
+#[must_use]
+pub fn process(input: &str) -> u64 {
+    input
+        .par_lines()
+        .filter_map(|line| match parse_game(line) {
+            Ok((_, g)) => {
+                if !g.sets.into_iter().any(|s| {
+                    s.into_iter().any(|c| match c.color {
+                        crate::Color::Red => c.number > BAG_RED,
+                        crate::Color::Green => c.number > BAG_GREEN,
+                        crate::Color::Blue => c.number > BAG_BLUE,
+                    })
+                }) {
+                    return Some(g.id);
+                }
+                None
             }
-            None
+            Err(err) => {
+                println!("{err}");
+                None
+            }
         })
-        .sum::<u64>())
+        .sum::<u64>()
 }
 
 #[cfg(test)]
@@ -33,9 +36,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() -> anyhow::Result<()> {
+    fn it_works() {
         let input = include_str!("../example.txt");
-        assert_eq!(8, process(input)?);
-        Ok(())
+        assert_eq!(8, process(input));
     }
 }
